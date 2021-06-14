80f4f836089ba4d2f3ea78e292a7b4bc31affde7
sailorj94 <jackniall9@gmail.com>
2021-06-14 16:59:32 +0100

    Theme Park extension in progress

--- src/main/java/ThemePark.java
+++ src/main/java/ThemePark.java
@@ -1,12 +1,46 @@
 import attractions.Attraction;
-import org.w3c.dom.Attr;
+import behaviours.IReviewed;
+import stalls.Stall;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class ThemePark {
 
-    ArrayList<Attraction> attractions;
-    ArrayList<Attraction> stalls;
+    private ArrayList<Attraction> attractions;
+    private ArrayList<Stall> stalls;
+    private ArrayList<IReviewed> reviewedAttractions;
+
+    public ThemePark(ArrayList<Attraction> attractions, ArrayList<Stall> stalls, ArrayList<IReviewed> reviewedItems) {
+        this.attractions = attractions;
+        this.stalls = stalls;
+        this.reviewedAttractions = reviewedItems;
+    }
+
+    public void addAttraction(Attraction attraction){
+        this.attractions.add(attraction);
+    }
+
+    public void addStall(Stall stall) {
+        this.stalls.add(stall);
+    }
+
+    public ArrayList returnAttractions(){
+        return this.attractions;
+    }
+
+    public ArrayList returnStalls(){
+        return this.stalls;
+    }
+
+    public ArrayList<IReviewed> getReviewedItems() {
+        return reviewedAttractions;
+    }
+
+
+
+
+
 
 
 }
